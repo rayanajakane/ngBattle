64cf5ddcacaed8c4acd9be1c2e1eaced465a6d6b
zakikero <zakikerouani@gmail.com>
2024-09-07 02:03:34 -0400

    Refactor admin-page component HTML and SCSS
    - Updated the HTML structure of the admin-page component to improve readability and organization.
    - Added CSS classes and styles to the admin-page component to enhance the visual appearance.
    - Created a new import-page component with its HTML, SCSS, and test files.
    - Updated the main.ts file to include routes for the create and import pages.

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,9 +6,10 @@ import { Routes, provideRouter } from '@angular/router';
 import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 //import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { CreatePageComponent } from '@app/pages/create-page/create-page.component';
+import { ImportPageComponent } from '@app/pages/import-page/import-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { environment } from './environments/environment';
-import { CreatePageComponent } from '@app/pages/create-page/create-page.component';
 
 import { AdminPageComponent } from 'src/app/pages/admin-page/admin-page.component';
 
@@ -16,16 +17,16 @@ if (environment.production) {
     enableProdMode();
 }
 
-
 const routes: Routes = [
+    //{ path: '**', redirectTo: '/home' },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: AdminPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
-    { path: '**', redirectTo: '/home' },
-    { path: 'createView', component: CreatePageComponent }
+    { path: 'home', component: AdminPageComponent },
+    { path: 'createView', component: CreatePageComponent },
+    { path: 'importView', component: ImportPageComponent },
 ];
 
 bootstrapApplication(AppComponent, {
     providers: [provideHttpClient(), provideRouter(routes), provideAnimations()],
-})
+});
