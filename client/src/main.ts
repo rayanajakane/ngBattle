8ef3cc38bca3f51450bc5375fd090e65a579aa0c
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2024-09-12 12:22:16 -0400

    feat: adding map component

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,7 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter } from '@angular/router';
+import { MapComponent } from '@app/components/map/map.component';
 import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
@@ -19,6 +20,7 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'map', component: MapComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
