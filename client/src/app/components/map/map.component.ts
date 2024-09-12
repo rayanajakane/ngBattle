90b564b6fc6ca1beec34a1f8126617cafa1a04d8
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2024-09-12 16:38:42 -0400

    feat(map): create and display grid with mat-grid-list

--- client/src/app/components/map/map.component.ts
+++ client/src/app/components/map/map.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatGridListModule, MatGridTile } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule, MatGridTile],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
+    @Input() mapSize : number = 15;
+    //
+    tiles : string[];
+    constructor( ) {
 
+        this.tiles = Array(this.mapSize*this.mapSize).fill('TUILEEE')
+
+    }
+
+
+    CreateGrid(mapSize: number) {
+
+    }
+
+
+    ngAfterViewInit() {}
 }
