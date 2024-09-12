c8bdbc91b1fa4f038d16ae7912e2f1216742402e
YanisLourdjane <yanis.lourdjane@polymtl.ca>
2024-09-12 14:26:53 -0400

    fix(admin-view): add submit button and delete duplacated files

--- client/src/app/pages/create-page/create-page.component.ts
+++ client/src/app/pages/create-page/create-page.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { GameMode } from '@app/data-structure/enum/game-mode-enum';
 import { SizeMap } from '@app/data-structure/enum/size-map-enum';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
     selector: 'app-create-page',
     standalone: true,
-    imports: [FormsModule, RouterLink],
+    imports: [FormsModule, RouterLink, CommonModule],
     templateUrl: './create-page.component.html',
     styleUrl: './create-page.component.scss',
 })
 export class CreatePageComponent {
     gameMode: GameMode = GameMode.Classic;
     sizeMap: SizeMap = SizeMap.Small;
+    nameGame: string = '';
+    descriptionGame: string = '';
 
-    // submitChoice() {
-    //     // console.log('Selected choice:', this.sizeMap);
-    //     // console.log('Selected choice:', this.gameMode);
-    // }
+    constructor(private router: Router) {}
+
+    submitChoice() {
+        console.log('Game Name:', this.nameGame);
+        console.log('Description:', this.descriptionGame);
+        console.log('Game Mode:', this.gameMode);
+        console.log('Map Size:', this.sizeMap);
+        // Navigate to another page if needed
+        this.router.navigate(['/edit']);
+    }
 }
