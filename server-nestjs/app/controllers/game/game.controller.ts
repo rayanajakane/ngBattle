f669eb0d36b6578926377e0d82b921d647e3b665
YanisLourdjane <yanis.lourdjane@polymtl.ca>
2024-09-12 19:45:09 -0400

    feat(game-saving): add Put request handler

--- server-nestjs/app/controllers/game/game.controller.ts
+++ server-nestjs/app/controllers/game/game.controller.ts
@@ -1,6 +1,6 @@
 import { GameDto } from '@app/model/dto/game/game.dto';
 import { GameService } from '@app/services/game/game.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
 
 @Controller('game')
 export class GameController {
@@ -11,5 +11,8 @@ export class GameController {
         return this.gameService.create(gameData);
     }
 
-
+    @Put('update')
+    updateGame(@Body() gameData: GameDto) {
+        return this.gameService.update(gameData);
+    }
 }
--- server-nestjs/app/services/game/game.service.ts
+++ server-nestjs/app/services/game/game.service.ts
@@ -1,24 +1,30 @@
-import { GameDto } from "@app/model/dto/game/game.dto";
-import { Game } from "@app/model/schema/game.schema";
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { GameDto } from '@app/model/dto/game/game.dto';
+import { Game } from '@app/model/schema/game.schema';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class GameService {
     constructor(@InjectModel(Game.name) private gameModel: Model<Game>) {}
 
     async create(createGameDto: GameDto): Promise<Game> {
-        const filteredGamesById: Game[] = await this.findById(createGameDto.id);
+        const filteredGamesById: Game[] = await this.gameModel.find({ id: createGameDto.id }).exec();
         if (filteredGamesById.length === 1) {
             const existingGame = filteredGamesById[0];
             await this.gameModel.updateOne({ _id: existingGame._id }, createGameDto).exec();
-            return this.gameModel.findById(existingGame._id).exec();
-        }
-        else {
+            return await this.gameModel.findById(existingGame._id).exec();
+        } else {
             const createdGame = new this.gameModel(createGameDto);
-            return createdGame.save();
+            return await createdGame.save();
         }
+    }
+
+    async update(createGameDto: GameDto): Promise<Game> {
+        const filteredGamesById: Game = await this.gameModel.findOne({ id: createGameDto.id }).exec();
+        const existingGame = filteredGamesById[0];
+        await this.gameModel.updateOne({ _id: existingGame._id }, createGameDto).exec();
+        return await this.gameModel.findById(existingGame._id).exec();
     }
 
     async findById(id: string): Promise<Game[]> {
