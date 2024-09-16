69c9a687809163520beef66048b3d0c0c66f7d8c
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2024-09-15 20:32:33 -0400

    feat: implement edit header in edit page

--- client/src/app/pages/edit-page/edit-page.component.ts
+++ client/src/app/pages/edit-page/edit-page.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 
 @Component({
     selector: 'app-edit-page',
     standalone: true,
-    imports: [ToolbarComponent, SidebarComponent, MatButtonModule],
+    imports: [ToolbarComponent, SidebarComponent, MatButtonModule, MatIcon, MatMenuModule, FormsModule, MatFormFieldModule, MatInputModule],
     templateUrl: './edit-page.component.html',
     styleUrl: './edit-page.component.scss',
 })
-export class EditPageComponent {}
+export class EditPageComponent {
+    gameTitle: string;
+    gameDescription: string;
+    constructor() {
+        this.gameTitle = '[ Game Title ]';
+        this.gameDescription = '[ Game Description ]';
+    }
+    changeHeader(title: string, description: string) {
+        this.gameTitle = title;
+        this.gameDescription = description;
+    }
+}
