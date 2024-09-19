658faf990a5b999e95fbbab3295b0a9b2d573399
YanisLourdjane <yanis.lourdjane@polymtl.ca>
2024-09-19 14:07:08 -0400

    feature(game-selection-page): create game selection page

---
+++ client/src/app/pages/game-selection-page/game-selection-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GameSelectionPageComponent } from './game-selection-page.component';
+
+describe('GameSelectionPageComponent', () => {
+  let component: GameSelectionPageComponent;
+  let fixture: ComponentFixture<GameSelectionPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GameSelectionPageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GameSelectionPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/pages/game-selection-page/game-selection-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-game-selection-page',
+    standalone: true,
+    imports: [],
+    templateUrl: './game-selection-page.component.html',
+    styleUrl: './game-selection-page.component.scss',
+})
+export class GameSelectionPageComponent {}
