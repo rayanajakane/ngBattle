3c8993712e5693997389505d3e73bd7526cab63f
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2024-09-19 14:57:06 -0400

    fix(create-page-btn): import MatButtonModule to load btn correctly

--- client/src/app/pages/create-page/create-page.component.ts
+++ client/src/app/pages/create-page/create-page.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
 import { Router, RouterLink } from '@angular/router';
@@ -8,14 +9,12 @@ import { Router, RouterLink } from '@angular/router';
 @Component({
     selector: 'app-create-page',
     standalone: true,
-    imports: [FormsModule, RouterLink, CommonModule, MatRadioModule, MatIconModule],
+    imports: [MatButtonModule, FormsModule, RouterLink, CommonModule, MatRadioModule, MatIconModule],
     templateUrl: './create-page.component.html',
     styleUrl: './create-page.component.scss',
 })
 export class CreatePageComponent {
     gameType: string;
-    gameName: string;
-    gameDescription: string;
     mapSize: string;
     constructor(private router: Router) {}
     submitChoice() {
