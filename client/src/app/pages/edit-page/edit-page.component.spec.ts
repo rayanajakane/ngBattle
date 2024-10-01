22fc666702b9eaf86f2bd6237ea357cb2d0aea0f
zakikero <zakikerouani@gmail.com>
2024-10-01 19:27:10 -0400

    refactor(mapComponent): added tests for full coverage of map component

--- client/src/app/pages/edit-page/edit-page.component.spec.ts
+++ client/src/app/pages/edit-page/edit-page.component.spec.ts
@@ -1,11 +1,18 @@
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import SpyObj = jasmine.SpyObj;
+
 import { provideRouter } from '@angular/router';
 
 import { EditPageComponent } from './edit-page.component';
+// import { MapService } from '@app/services/map.service';
+// import { IDGenerationService } from '@app/services/idgeneration.service';
 
 describe('EditPageComponent', () => {
+    // let mapServiceSpy: SpyObj<MapService>;
+    // let idGenerationServiceSpy: SpyObj<IDGenerationService>;
+
     let component: EditPageComponent;
     let fixture: ComponentFixture<EditPageComponent>;
 
