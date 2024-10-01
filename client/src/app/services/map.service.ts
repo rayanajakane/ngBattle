7a37021152a576636ba00e63f7b47c1573fad618
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2024-10-01 15:54:44 -0400

    test

--- client/src/app/services/map.service.ts
+++ client/src/app/services/map.service.ts
@@ -8,24 +8,22 @@ import { TileTypes } from '@app/data-structure/toolType';
 })
 export class MapService {
     // Optionally put a map if we import a map
-    createGrid(mapSize?: number, tiles?: TileJson[]): TileJson[] {
+    createGrid(mapSize?: number): TileJson[] {
         if (mapSize !== undefined && mapSize <= 0) {
             throw new Error('MapSize must be a positive number.');
         }
         const arraySize = mapSize ? mapSize : DEFAULT_MAP_SIZE;
-        return tiles
-            ? tiles
-            : Array(arraySize * arraySize)
-                  .fill(0)
-                  .map((_, index) => {
-                      // Assign a unique id based on the index
-                      return {
-                          idx: index, // Unique ID for each tile
-                          tileType: TileTypes.BASIC, // Tile type
-                          item: '',
-                          hasPlayer: false,
-                      };
-                  });
+        return Array(arraySize * arraySize)
+            .fill(0)
+            .map((_, index) => {
+                // Assign a unique id based on the index
+                return {
+                    idx: index, // Unique ID for each tile
+                    tileType: TileTypes.BASIC, // Tile type
+                    item: '',
+                    hasPlayer: false,
+                };
+            });
     }
     chooseTileType(currentTileType: string, newTileType: string): string {
         if (newTileType === TileTypes.DOOR) {
