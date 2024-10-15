f83f7d7058951c8d3b2efb5c678ddfebeae1f29d
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2024-10-15 00:51:06 -0400

    feat(game-page): add player list to view

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
 import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { TileJson } from '@app/data-structure/game-structure';
@@ -12,7 +15,17 @@ import { MapComponent } from '../../components/map/map.component';
     standalone: true,
     templateUrl: './game-page.component.html',
     styleUrls: ['./game-page.component.scss'],
-    imports: [SidebarComponent, PlayAreaComponent, MapComponent, MatButtonModule, MatTabsModule, MatGridListModule],
+    imports: [
+        MatToolbarModule,
+        MatListModule,
+        MatDividerModule,
+        SidebarComponent,
+        PlayAreaComponent,
+        MapComponent,
+        MatButtonModule,
+        MatTabsModule,
+        MatGridListModule,
+    ],
 })
 export class GamePageComponent {
     mapSize: number = 10;
