2fa2c377151b52bc0075fe18579016d977ce0d6a
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2024-10-15 00:27:45 -0400

    feat(game-page): implement initial view elements

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTabsModule } from '@angular/material/tabs';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
 import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
+import { TileJson } from '@app/data-structure/game-structure';
+import { MapComponent } from '../../components/map/map.component';
 
 @Component({
     selector: 'app-game-page',
     standalone: true,
     templateUrl: './game-page.component.html',
     styleUrls: ['./game-page.component.scss'],
-    imports: [SidebarComponent, PlayAreaComponent],
+    imports: [SidebarComponent, PlayAreaComponent, MapComponent, MatButtonModule, MatTabsModule, MatGridListModule],
 })
-export class GamePageComponent {}
+export class GamePageComponent {
+    mapSize: number = 10;
+    gameMap: TileJson[];
+    ngOnInit() {
+        this.gameMap = Array(this.mapSize * this.mapSize).fill({ tileType: '' });
+    }
+}
