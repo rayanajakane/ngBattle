01609d0f93036ba7335e374864178772fecb4a6b
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2024-10-17 12:06:56 -0400

    added map preview to gameSelection

--- client/src/app/components/game-selection/game-selection.component.ts
+++ client/src/app/components/game-selection/game-selection.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { GameJson } from '@app/data-structure/game-structure';
+import { MapPreviewComponent } from '../map-preview/map-preview.component';
 
 @Component({
     selector: 'app-game-selection',
     standalone: true,
-    imports: [RouterLink],
+    imports: [RouterLink, MapPreviewComponent],
     templateUrl: './game-selection.component.html',
     styleUrl: './game-selection.component.scss',
 })
 export class GameSelectionComponent {
     @Input() game: GameJson;
+
+    getMapSize(): number {
+        return parseInt(this.game.mapSize, 10);
+    }
 }
