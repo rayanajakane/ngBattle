b8d0dfc468928e1cf0d5761a3a2d20663942cb9b
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2024-11-01 22:03:57 -0400

    feat(previsualize): added methods to change shortest path based on tile hovered

--- client/src/app/services/map-game.service.ts
+++ client/src/app/services/map-game.service.ts
@@ -7,13 +7,27 @@ import { MapBaseService } from './map-base.service';
 })
 export class MapGameService extends MapBaseService {
     tiles: GameTile[];
+    accessibleTiles: number[] = [];
+    tilesShortestPaths: { [key: number]: number[] } = {};
+
+    constructor() {
+        super();
+        this.fetchAccessibleTiles();
+        this.fetchShortestPath();
+    }
 
     onRightClick(index: number): void {}
     onMouseDown(index: number, event: MouseEvent): void {}
     onMouseUp(index: number, event: MouseEvent): void {}
     onDrop(index: number): void {}
-    onMouseEnter(index: number, event: MouseEvent): void {}
-    onExit(): void {}
+    onMouseEnter(index: number, event: MouseEvent): void {
+        event.preventDefault();
+        this.setAccessibleTiles();
+        this.setShortestPath(index);
+    }
+    onExit(): void {
+        this.removeAllPreview();
+    }
 
     setPreview(indexes: number[], previewType: TilePreview): void {
         indexes.forEach((index) => {
@@ -25,5 +39,31 @@ export class MapGameService extends MapBaseService {
         this.tiles.forEach((tile) => {
             tile.isAccessible = TilePreview.NONE;
         });
+    }
+
+    //temp function to test accessible tiles
+    fetchAccessibleTiles(): void {
+        this.accessibleTiles = Array.from({ length: 51 }, (_, i) => i);
+    }
+
+    //temp function to test shortest path
+    fetchShortestPath(): void {
+        this.tilesShortestPaths[1] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        this.tilesShortestPaths[2] = [10, 11, 12, 13, 14, 15, 16, 17, 18];
+        this.tilesShortestPaths[3] = [19, 20, 21, 22, 23, 24, 25, 26, 27];
+    }
+
+    resetShortestPath(): void {
+        this.tilesShortestPaths = {};
+    }
+
+    setShortestPath(index: number): void {
+        if (this.tilesShortestPaths[index]) {
+            this.setPreview(this.tilesShortestPaths[index], TilePreview.SHORTESTPATH);
+        }
+    }
+
+    setAccessibleTiles(): void {
+        this.setPreview(this.accessibleTiles, TilePreview.PREVIEW);
     }
 }
