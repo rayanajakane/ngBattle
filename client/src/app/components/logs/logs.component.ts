a5cfbd8a836d88230ae5225b257692ae239ba44f
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2024-11-04 16:03:07 -0500

    feat: implement LogComponent

--- client/src/app/components/logs/logs.component.ts
+++ client/src/app/components/logs/logs.component.ts
@@ -1,52 +1,56 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { LogMessage } from '@app/interfaces/message';
-import { SocketService } from '@app/services/socket.service';
+//import { SocketService } from '@app/services/socket.service';
 
 @Component({
     selector: 'app-logs',
     standalone: true,
-    imports: [FormsModule, CommonModule],
+    imports: [MatCardModule, MatChipsModule, MatProgressBarModule, FormsModule, CommonModule],
     templateUrl: './logs.component.html',
     styleUrl: './logs.component.scss',
 })
-export class LogsComponent implements OnInit {
-    @ViewChild('messageContainer') messageContainer: ElementRef;
+export class LogsComponent {
+    @ViewChild('logsContainer') logsContainer: ElementRef;
     @Input() roomId: string;
 
-    logs: LogMessage[] = [];
+    logs: LogMessage[] = [{ date: '2023-10-01', message: 'Log message 1' }];
 
-    constructor(
-        private socketService: SocketService,
-        private cdr: ChangeDetectorRef,
-    ) {}
+    constructor() {} // private cdr: ChangeDetectorRef, // private socketService: SocketService,
 
-    ngOnInit() {
-        this.loadLogs();
-        this.receiveLog();
+    ngOnInit(): void {
+        //     this.loadLogs();
+        //     this.receiveLog();
     }
 
-    loadLogs() {
-        this.socketService.once('loadAllLogs', (data: { logs: LogMessage[] }) => {
-            this.logs = data.logs;
-        });
-        this.socketService.emit('loadAllLogs', { roomId: this.roomId });
-    }
+    // ngAfterViewInit() {
+    //     this.scrollToBottom();
+    // }
 
-    receiveLog() {
-        this.socketService.on('newLog', (log: LogMessage) => {
-            this.logs.push(log);
-            this.cdr.detectChanges();
-            this.scrollToBottom();
-        });
-    }
+    // loadLogs() {
+    //     this.socketService.once('loadAllLogs', (data: { logs: LogMessage[] }) => {
+    //         this.logs = data.logs;
+    //     });
+    //     this.socketService.emit('loadAllLogs', { roomId: this.roomId });
+    // }
+
+    // receiveLog() {
+    //     this.socketService.on('newLog', (log: LogMessage) => {
+    //         this.logs.push(log);
+    //         this.cdr.detectChanges();
+    //         this.scrollToBottom();
+    //     });
+    // }
 
     scrollToBottom() {
         try {
-            this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
+            this.logsContainer.nativeElement.scrollTop = this.logsContainer.nativeElement.scrollHeight;
         } catch (err) {
-            console.error('Scroll to bottom failed', err);
+            console.error('Scroll to bottom failed in logsComponent', err);
         }
     }
 }
