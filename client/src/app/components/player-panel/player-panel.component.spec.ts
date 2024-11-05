2c64e9d9e7aec5d3e9b5910e26d12d41dbec1dcf
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2024-11-05 10:35:54 -0500

    test: implement tests for player-panel component

--- client/src/app/components/player-panel/player-panel.component.spec.ts
+++ client/src/app/components/player-panel/player-panel.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PlayerPanelComponent } from './player-panel.component';
 
 describe('PlayerPanelComponent', () => {
@@ -18,5 +17,29 @@ describe('PlayerPanelComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should set bonusAttackDice to D6 if bonusDice is attack', () => {
+        component.bonusDice = 'attack';
+        component.ngOnInit();
+        expect(component.bonusAttackDice).toBe('D6');
+    });
+
+    it('should set bonusDefenseDice to D6 if bonusDice is defense', () => {
+        component.bonusDice = 'defense';
+        component.ngOnInit();
+        expect(component.bonusDefenseDice).toBe('D6');
+    });
+
+    it('should not change bonusAttackDice if bonusDice is not attack', () => {
+        component.bonusDice = 'defense';
+        component.ngOnInit();
+        expect(component.bonusAttackDice).toBe('D4');
+    });
+
+    it('should not change bonusDefenseDice if bonusDice is not defense', () => {
+        component.bonusDice = 'attack';
+        component.ngOnInit();
+        expect(component.bonusDefenseDice).toBe('D4');
     });
 });
