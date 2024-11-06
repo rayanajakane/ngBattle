863bf06f1784d524624bd575f6b53caf6e455961
Ali Gabr <aligabr03@gmail.com>
2024-11-06 02:23:34 +0000

    Update character-selection-page.component.ts

--- client/src/app/pages/character-selection-page/character-selection-page.component.ts
+++ client/src/app/pages/character-selection-page/character-selection-page.component.ts
@@ -50,7 +50,6 @@ export class CharacterSelectionPageComponent {
 
     formChecking(): string[] {
         const errors: string[] = [];
-        // Vérification des erreurs
         if (!this.selectedAvatar) errors.push('- Veuillez sélectionner un avatar avant de continuer');
         if (!this.isNameValid()) errors.push('- Veuillez mettre un nom pour le personne entre 3 et 15 charactères');
 
