b8bbabf3e2389cabecf329e0d40d9fbf2a670786
Ali Gabr <aligabr03@gmail.com>
2024-11-14 16:11:19 -0500

    test(player-stats): player stat tests done

--- client/src/app/components/player-stats/player-stats.component.spec.ts
+++ client/src/app/components/player-stats/player-stats.component.spec.ts
@@ -1,23 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlayerStatsComponent } from './player-stats.component';
 
 describe('PlayerStatsComponent', () => {
-  let component: PlayerStatsComponent;
-  let fixture: ComponentFixture<PlayerStatsComponent>;
+    let component: PlayerStatsComponent;
+    let fixture: ComponentFixture<PlayerStatsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PlayerStatsComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [PlayerStatsComponent, MatTableModule, MatSortModule, BrowserAnimationsModule],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(PlayerStatsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(PlayerStatsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
