35a7cc72a5901742719723663c21a4e309b1ebcd
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2024-11-13 21:11:02 -0500

    git commit -m "feat(combat-gateway): implement `action` event
    Co-authored-by: Nisa <122929098+coinmaster2133@users.noreply.github.com>
    Co-authored-by: Samyar Janatian <samyar.mahmoudi@polymtl.ca>"

--- server/app/gateways/combat/combat.gateway.ts
+++ server/app/gateways/combat/combat.gateway.ts
@@ -1,6 +1,8 @@
 import { ActionButtonService } from '@app/services/action-button/action-button.service';
+import { ActionHandlerService } from '@app/services/action-handler/action-handler.service';
 import { ActiveGamesService } from '@app/services/active-games/active-games.service';
 import { CombatService } from '@app/services/combat/combat.service';
+import { TileTypes } from '@common/tile-types';
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 @WebSocketGateway()
@@ -11,6 +13,7 @@ export class CombatGateway {
         private readonly activeGameService: ActiveGamesService,
         private readonly combatService: CombatService,
         private readonly actionButtonService: ActionButtonService,
+        private readonly actionHandlerService: ActionHandlerService,
     ) {
         console.log('Combat Gateway created');
     }
@@ -19,17 +22,22 @@ export class CombatGateway {
     handleStartAction(@ConnectedSocket() client, @MessageBody() data: { roomId: string; playerId: string }) {
         const playerCoord = this.activeGameService.getActiveGame(data.roomId).playersCoord.find((player) => player.player.id === data.playerId);
         client.emit('startAction', this.actionButtonService.getAvailableIndexes(data.roomId, playerCoord));
-        // TODO: set the socket response to the client or all clients in the room
     }
 
     @SubscribeMessage('action')
     handleAction(@ConnectedSocket() client, @MessageBody() data: { roomId: string; playerId: string; target: number }) {
         // TODO: identify the target player in the front end to send a start action message instead of interpreting the target here (cant know if an item is targeted or a player)
-        const playerCoord = this.activeGameService.getActiveGame(data.roomId).playersCoord.find((player) => player.player.id === data.playerId);
-        const targetPlayer = this.activeGameService.getActiveGame(data.roomId).playersCoord.find((player) => player.position === data.target);
-
-        this.combatService.startCombat(data.roomId, [playerCoord, targetPlayer]);
-
+        const player = this.activeGameService.getActiveGame(data.roomId).playersCoord.find((player) => player.player.id === data.playerId);
+        if (this.activeGameService.getActiveGame(data.roomId).game.map[data.target].hasPlayer) {
+            const targetPlayer = this.activeGameService.getActiveGame(data.roomId).playersCoord.find((player) => player.position === data.target);
+        }
+        if (
+            this.activeGameService.getActiveGame(data.roomId).game.map[data.target].tileType === TileTypes.DOORCLOSED ||
+            this.activeGameService.getActiveGame(data.roomId).game.map[data.target].tileType === TileTypes.DOOROPEN
+        ) {
+            const newData = { roomId: data.roomId, playerId: data.playerId, doorPosition: data.target };
+            this.actionHandlerService.handleInteractDoor(newData, this.server, client);
+        }
         // TODO: set the socket response to the client or all clients in the room
     }
 
