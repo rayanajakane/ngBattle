f68be57600ec06de913d12797398ed3f6352a0cc
YanisLourdjane <yanis.lourdjane@polymtl.ca>
2024-11-15 17:36:06 -0500

    test(end-game-page): added imports and providers to game-end-page tests

--- client/src/app/pages/game-end-page/game-end-page.component.spec.ts
+++ client/src/app/pages/game-end-page/game-end-page.component.spec.ts
@@ -1,23 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { GameEndPageComponent } from './game-end-page.component';
 
 describe('GameEndPageComponent', () => {
-  let component: GameEndPageComponent;
-  let fixture: ComponentFixture<GameEndPageComponent>;
+    let component: GameEndPageComponent;
+    let fixture: ComponentFixture<GameEndPageComponent>;
+    let mockActivatedRoute: jasmine.SpyObj<ActivatedRoute>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GameEndPageComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        mockActivatedRoute = jasmine.createSpyObj('ActivatedRoute', [], {
+            params: of({ roomId: 'testRoom', characterName: 'testName' }),
+        });
 
-    fixture = TestBed.createComponent(GameEndPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        await TestBed.configureTestingModule({
+            imports: [GameEndPageComponent, BrowserAnimationsModule],
+            providers: [{ provide: ActivatedRoute, useValue: mockActivatedRoute }],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(GameEndPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
