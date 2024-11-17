901b20197970c4f0bf344d8b7183b82c5c2899a1
Nisa <122929098+Coinmaster2133@users.noreply.github.com>
2024-11-17 15:24:41 -0500

    tests: add a mock of each method of each service & current service methods test

--- server/app/services/action-button/action-button.service.spec.ts
+++ server/app/services/action-button/action-button.service.spec.ts
@@ -1,8 +1,10 @@
+import { GameInstance } from '@app/data-structures/game-instance';
 import { ActionService } from '@app/services/action/action.service';
 import { ActiveGamesService } from '@app/services/active-games/active-games.service';
 import { CombatService } from '@app/services/combat/combat.service';
 import { GameService } from '@app/services/game.service';
 import { MovementService } from '@app/services/movement/movement.service';
+import { GameStructure, TileStructure } from '@common/game-structure';
 import { Player, PlayerCoord } from '@common/player';
 import { TileTypes } from '@common/tile-types';
 import { Test, TestingModule } from '@nestjs/testing';
@@ -12,6 +14,7 @@ describe('ActionButtonService', () => {
     let service: ActionButtonService;
     let activeGamesService: ActiveGamesService;
     let combatService: CombatService;
+    let actionService: ActionService;
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [
@@ -22,16 +25,35 @@ describe('ActionButtonService', () => {
                         getDoorsAround: jest.fn(),
                     },
                 },
+                {
+                    provide: ActionService,
+                    useValue: {
+                        interactWithDoor: jest.fn(),
+                        startCombat: jest.fn(),
+                        movePlayer: jest.fn(),
+                        performAction: jest.fn(),
+                    },
+                },
+                {
+                    provide: ActiveGamesService,
+                    useValue: {
+                        getActiveGame: jest.fn(),
+                    },
+                },
+                {
+                    provide: CombatService,
+                    useValue: {
+                        startCombat: jest.fn(),
+                    },
+                },
                 ActionButtonService,
-                ActiveGamesService,
-                ActionService,
-                CombatService,
                 MovementService,
             ],
         }).compile();
         service = module.get<ActionButtonService>(ActionButtonService);
         activeGamesService = module.get<ActiveGamesService>(ActiveGamesService);
         combatService = module.get<CombatService>(CombatService);
+        actionService = module.get<ActionService>(ActionService);
     });
 
     it('should be defined', () => {
@@ -52,46 +74,6 @@ describe('ActionButtonService', () => {
         expect(result).toEqual([6, 7]);
     });
 
-    // An actual game instance is needed to test this function
-
-    // it('should return players around the given position', () => {
-    //     // Arrange
-    //     const roomId = '123abc';
-    //     const position = 5;
-    //     const mockPlayersAround = [
-    //         { player: { id: 'playerId' }, position: 6 },
-    //         { player: { id: 'opponentId' }, position: 4 },
-    //     ] as PlayerCoord[];
-
-    //     const game: GameStructure = {
-    //         id: 'gameId',
-    //         gameName: 'Test Game',
-    //         gameDescription: 'This is a test game',
-    //         mapSize: '10x10',
-    //         map: [
-    //             // Define your tile structures here
-    //         ],
-    //         gameType: 'Test',
-    //         isVisible: true,
-    //         creationDate: '2022-01-01',
-    //         lastModified: '2022-01-02',
-    //     };
-
-    //     const mockActiveGame: GameInstance = {
-    //         roomId: roomId,
-    //         game: game,
-    //         playersCoord: mockPlayersAround,
-    //     };
-
-    //     jest.spyOn(activeGamesService, 'getActiveGame').mockReturnValue(mockActiveGame);
-
-    //     // Act
-    //     const result = service.getPlayersAround(roomId, position);
-
-    //     // Assert
-    //     expect(result).toEqual(mockPlayersAround);
-    //     expect(activeGamesService.getActiveGame).toHaveBeenCalledWith(roomId);
-    // });
     it('should start combat with the given fighters', () => {
         const roomId = '123abc';
         const fighters: PlayerCoord[] = [
@@ -106,5 +88,96 @@ describe('ActionButtonService', () => {
         service.startCombat(roomId, fighters);
 
         expect(combatService.startCombat).toHaveBeenCalledWith(roomId, fighters);
+    });
+    it('should interact with door if tile is a door', () => {
+        const roomId = '123abc';
+        const originalPlayer: PlayerCoord = { player: { id: 'playerId' } as Player, position: 5 } as PlayerCoord;
+        const tileIndex = 0;
+
+        const mockMap = [
+            {
+                idx: 0,
+                tileType: TileTypes.DOORCLOSED,
+                item: '',
+                hasPlayer: false,
+            },
+            // ... other tiles
+            {
+                idx: 1,
+                tileType: 'doorClosed',
+                item: '',
+                hasPlayer: false,
+            },
+            // ... other tiles
+        ] as TileStructure[];
+
+        const game: GameStructure = {
+            id: 'f01d532b-31f8-4158-9398-29a03c39e646',
+            gameName: 'BRUH',
+            gameDescription: 'One day...',
+            mapSize: '10',
+            map: mockMap,
+            gameType: 'classic',
+            isVisible: true,
+            creationDate: '2024-11-11T19:59:26.518Z',
+            lastModified: '16/11/2024, 12:39:04',
+        };
+
+        jest.spyOn(activeGamesService, 'getActiveGame').mockReturnValue({
+            game: game,
+        } as GameInstance);
+
+        jest.spyOn(actionService, 'interactWithDoor');
+
+        service.chosenAction(roomId, originalPlayer, tileIndex);
+
+        expect(actionService.interactWithDoor).toHaveBeenCalledWith(roomId, originalPlayer.player.id, originalPlayer.position);
+    });
+
+    it('should start combat if tile has a player', () => {
+        const roomId = '123abc';
+        const originalPlayer: PlayerCoord = { player: { id: 'playerId' } as Player, position: 0 } as PlayerCoord;
+        const tileIndex = 1;
+
+        const mockMap = [
+            {
+                idx: 0,
+                tileType: TileTypes.ICE,
+                item: '',
+                hasPlayer: false,
+            },
+            {
+                idx: 1,
+                tileType: TileTypes.WALL,
+                item: '',
+                hasPlayer: true,
+            },
+            // ... other tiles
+        ] as TileStructure[];
+
+        const game: GameStructure = {
+            id: 'f01d532b-31f8-4158-9398-29a03c39e646',
+            gameName: 'BRUH',
+            gameDescription: 'One day...',
+            mapSize: '10',
+            map: mockMap,
+            gameType: 'classic',
+            isVisible: true,
+            creationDate: '2024-11-11T19:59:26.518Z',
+            lastModified: '16/11/2024, 12:39:04',
+        };
+
+        const targetPlayer: PlayerCoord = { player: { id: 'opponentId' } as Player, position: 1 } as PlayerCoord;
+
+        jest.spyOn(activeGamesService, 'getActiveGame').mockReturnValue({
+            game: game,
+            playersCoord: [originalPlayer, targetPlayer],
+        } as GameInstance);
+
+        jest.spyOn(service, 'startCombat');
+
+        service.chosenAction(roomId, originalPlayer, tileIndex);
+
+        expect(service.startCombat).toHaveBeenCalledWith(roomId, [originalPlayer, targetPlayer]);
     });
 });
