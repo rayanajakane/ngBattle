02c5eea66ed89b03d463964b638cf966747dc702
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2024-11-19 17:22:24 -0500

    chores(classic-game): remove unused tests and methods

--- client/src/app/components/timer/timer.component.spec.ts
+++ client/src/app/components/timer/timer.component.spec.ts
@@ -1,5 +1,5 @@
-import { ComponentFixture, discardPeriodicTasks, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { COUNTDOWN_DELAY, TIME_LEFT } from '@app/components/timer/constant';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TIME_LEFT } from '@app/components/timer/constant';
 import { TimerState } from '@common/game-structure';
 import { TimerComponent } from './timer.component';
 
@@ -26,54 +26,6 @@ describe('TimerComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
-
-    it('should start the timer if it is not running', fakeAsync(() => {
-        component.isRunning = false;
-        component.startTimer();
-        expect(component.isRunning).toBeTrue();
-        expect(component.isActive).toBeTrue();
-        expect(component.timerSubscription).not.toBeNull();
-        tick(COUNTDOWN_DELAY); // Flush any pending timers
-        component.stopTimer();
-        discardPeriodicTasks();
-    }));
-
-    it('should stop the existing timer and start a new one if it is already running', fakeAsync(() => {
-        component.isRunning = true;
-        const stopTimerSpy = spyOn(component, 'stopTimer').and.callThrough();
-        component.startTimer();
-        expect(stopTimerSpy).toHaveBeenCalled();
-        expect(component.isRunning).toBeTrue();
-        expect(component.isActive).toBeTrue();
-        expect(component.timerSubscription).not.toBeNull();
-        component.stopTimer();
-        discardPeriodicTasks();
-    }));
-    it('should stop the timer', fakeAsync(() => {
-        component.startTimer();
-        tick(COUNTDOWN_DELAY);
-        component.stopTimer();
-        expect(component.isRunning).toBeFalse();
-        expect(component.isActive).toBeFalse();
-        expect(component.timerSubscription).toBeNull();
-    }));
-
-    it('should decrement timeLeft until it reaches zero', fakeAsync(() => {
-        component.startTimer();
-        tick(COUNTDOWN_DELAY * (TIME_LEFT + 1)); // Ensure we go past zero
-        expect(component.timeLeft).toBe(0);
-        expect(component.isRunning).toBeFalse();
-        expect(component.isActive).toBeFalse();
-    }));
-
-    it('should clean up on destroy', fakeAsync(() => {
-        component.startTimer();
-        tick(COUNTDOWN_DELAY); // Allow some time for the timer to start
-        component.ngOnDestroy();
-        expect(component.isRunning).toBeFalse();
-        expect(component.isActive).toBeFalse();
-        expect(component.timerSubscription).toBeNull();
-    }));
 
     it('should set timerStateEnum to "Jeu" when timerState is REGULAR', () => {
         component.timerState = TimerState.REGULAR;
--- client/src/app/components/timer/timer.component.ts
+++ client/src/app/components/timer/timer.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { COUNTDOWN_DELAY, TIME_LEFT } from '@app/components/timer/constant';
+import { TIME_LEFT } from '@app/components/timer/constant';
 import { TimerState } from '@common/game-structure';
-import { interval, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-timer',
@@ -13,7 +13,7 @@ import { interval, Subscription } from 'rxjs';
     styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnChanges {
-    @Input() timeLeft: number = TIME_LEFT; // Set the initial time in seconds
+    @Input() timeLeft: number | '--' = TIME_LEFT; // Set the initial time in seconds
     timerSubscription: Subscription | null = null;
     isRunning: boolean = false;
     isActive: boolean = false;
@@ -40,35 +40,5 @@ export class TimerComponent implements OnChanges {
                 this.timerStateEnum = '';
                 break;
         }
-    }
-
-    startTimer() {
-        if (this.isRunning) {
-            this.stopTimer(); // Stop the existing timer if it's running
-        }
-        this.isActive = true;
-        this.isRunning = true;
-
-        this.timerSubscription = interval(COUNTDOWN_DELAY).subscribe(() => {
-            if (this.timeLeft > 0) {
-                this.timeLeft--;
-            } else {
-                this.stopTimer(); // Stop the timer when it reaches zero
-            }
-        });
-    }
-
-    // this can be called at the end of the turn.
-    stopTimer() {
-        if (this.timerSubscription) {
-            this.timerSubscription.unsubscribe();
-            this.timerSubscription = null;
-        }
-        this.isRunning = false;
-        this.isActive = false;
-    }
-
-    ngOnDestroy() {
-        this.stopTimer();
     }
 }
