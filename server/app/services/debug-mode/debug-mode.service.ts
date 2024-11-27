651ed779635c5e73171bfeebc43e9bc8aedee3bb
Nisa <122929098+Coinmaster2133@users.noreply.github.com>
2024-11-26 23:35:01 -0500

    feat: logs for debug & server side listeners

--- server/app/services/debug-mode/debug-mode.service.ts
+++ server/app/services/debug-mode/debug-mode.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DebugModeService {
-    private debugMode = false;
+    private isDebugMode = false;
 
-    toggleDebugMode(): void {
-        this.debugMode = !this.debugMode;
-        console.log('Debug mode is now ' + (this.debugMode ? 'active' : 'inactive'));
+    debugModeOn(): void {
+        this.isDebugMode = true;
     }
 
+    debugModeOff(): void {
+        this.isDebugMode = false;
+    }
     isDebugModeActive(): boolean {
-        return this.debugMode;
+        return this.isDebugMode;
     }
 }
