7119b5a960ad545979fdeae124d3415ea50e0041
zakikero <zakikerouani@gmail.com>
2024-11-28 21:17:08 -0500

    feat(ctf):  rafactored logic to make the flag item in the edit view to only apear on ctf mode
    Co-authored-by: YanisLourdjane <YanisLourdjane@users.noreply.github.com>

--- server/app/data-structures/game-instance.ts
+++ server/app/data-structures/game-instance.ts
@@ -2,9 +2,10 @@ import { CombatTimerService } from '@app/services/combat-timer/combat-timer.serv
 import { TimerService } from '@app/services/timer/timer.service';
 import { GameStructure } from '@common/game-structure';
 import { PlayerCoord } from '@common/player';
-
+import { GameMode } from '@common/game-structure';
 export interface GameInstance {
     roomId: string;
+    // gameMode: GameMode;
     game: GameStructure;
     turnTimer?: TimerService;
     combatTimer?: CombatTimerService;
