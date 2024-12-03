03bd5d4dec6a0b4ff2067ad6b6606c809fc6fe35
Nisa <122929098+Coinmaster2133@users.noreply.github.com>
2024-12-03 16:38:29 -0500

    lints: corrected all lints client side

---
+++ client/src/app/components/virtual-player-dialog/virtual-player-dialog.utils.ts
@@ -0,0 +1,7 @@
+export const HEIGHT = 8;
+export const MINUS_TWO = -2;
+export const MINUS_ONE = -1;
+export const TWO = 2;
+export const SIX = 6;
+export const FOUR = 4;
+export const ZERO_POINT_FIVE = 0.5;
