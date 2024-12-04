b342fc1ba7246000ff21c29646081963968bd529
Ali Gabr <aligabr03@gmail.com>
2024-12-03 23:03:26 -0500

    style: Game selection page slider uses admin item component

--- client/src/app/components/game-selection/game-selection.component.ts
+++ client/src/app/components/game-selection/game-selection.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 import { MapPreviewComponent } from '@app/components/map-preview/map-preview.component';
 import { GameStructure } from '@common/game-structure';
@@ -6,7 +8,7 @@ import { GameStructure } from '@common/game-structure';
 @Component({
     selector: 'app-game-selection',
     standalone: true,
-    imports: [RouterLink, MapPreviewComponent],
+    imports: [RouterLink, MapPreviewComponent, MatCardModule, MatTooltipModule],
     templateUrl: './game-selection.component.html',
     styleUrl: './game-selection.component.scss',
 })
